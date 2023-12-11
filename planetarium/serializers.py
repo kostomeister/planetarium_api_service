6cafd6ded18419360c48abfd9c6b7c8963522e84
Kostia <kostia3507@gmail.com>
2023-12-11 16:48:50 +0200

    added serializers for tickets

--- planetarium/serializers.py
+++ planetarium/serializers.py
@@ -1,6 +1,7 @@
+from django.core.exceptions import ValidationError
 from rest_framework import serializers
 
-from .models import PlanetariumDome, ShowTheme, AstronomyShow, ShowSession
+from .models import PlanetariumDome, ShowTheme, AstronomyShow, ShowSession, Ticket
 
 
 class PlanetariumDomeSerializer(serializers.ModelSerializer):
@@ -54,6 +55,32 @@ class ShowSessionListSerializer(ShowSessionSerializer):
         read_only=True,
         slug_field="name"
     )
+
+
+class TicketSerializer(serializers.ModelSerializer):
+    def validate(self, attrs):
+        data = super(TicketSerializer, self).validate(attrs=attrs)
+        Ticket.validate_ticket(
+            attrs["row"],
+            attrs["seat"],
+            attrs["show_session"].planetarium_dome,
+            ValidationError
+        )
+        return data
+
+    class Meta:
+        model = Ticket
+        fields = ("id", "row", "seat", "show_session")
+
+
+class TicketListSerializer(TicketSerializer):
+    movie_session = ShowSessionListSerializer(many=False, read_only=True)
+
+
+class TicketSeatsSerializer(TicketSerializer):
+    class Meta:
+        model = Ticket
+        fields = ("row", "seat")
 
 
 class ShowSessionDetailSerializer(ShowSessionSerializer):
