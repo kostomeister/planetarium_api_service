e89c8c623bf9bcb2967cffc0beb3928747432341
Kostia <kostia3507@gmail.com>
2023-12-11 15:54:38 +0200

    added show session view set to urls file

--- planetarium/urls.py
+++ planetarium/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from planetarium.views import PlanetariumDomeViewSet, ShowThemeViewSet, AstronomyShowViewSet
+from planetarium.views import PlanetariumDomeViewSet, ShowThemeViewSet, AstronomyShowViewSet, ShowSessionViewSet
 
 router = routers.DefaultRouter()
 
 router.register("planetarium_dome", PlanetariumDomeViewSet)
 router.register("show_theme", ShowThemeViewSet)
 router.register("astronomy_show", AstronomyShowViewSet)
+router.register("show_session", ShowSessionViewSet)
 
 urlpatterns = [
     path("", include(router.urls))
