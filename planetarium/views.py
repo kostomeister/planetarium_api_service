f3736f1be0604fd5e89ec652bf3b34d9c462843b
Kostia <kostia3507@gmail.com>
2023-12-11 21:32:12 +0200

    added permission classes to all views

--- planetarium/views.py
+++ planetarium/views.py
@@ -1,6 +1,13 @@
 from rest_framework import viewsets, mixins
+from rest_framework.permissions import IsAuthenticated
 
-from .models import PlanetariumDome, ShowTheme, AstronomyShow, ShowSession, Reservation
+from .models import (
+    PlanetariumDome,
+    ShowTheme,
+    AstronomyShow,
+    ShowSession,
+    Reservation
+)
 from .serializers import (
     PlanetariumDomeSerializer,
     ShowThemeSerializer,
@@ -13,6 +20,7 @@ from .serializers import (
     ReservationSerializer,
     ReservationListSerializer
 )
+from .permissions import IsAdminOrIfAuthenticatedReadOnly
 
 
 class PlanetariumDomeViewSet(
@@ -22,6 +30,7 @@ class PlanetariumDomeViewSet(
 ):
     queryset = PlanetariumDome.objects.all()
     serializer_class = PlanetariumDomeSerializer
+    permission_classes = (IsAdminOrIfAuthenticatedReadOnly, )
 
 
 class ShowThemeViewSet(
@@ -31,11 +40,13 @@ class ShowThemeViewSet(
 ):
     queryset = ShowTheme.objects.all()
     serializer_class = ShowThemeSerializer
+    permission_classes = (IsAdminOrIfAuthenticatedReadOnly, )
 
 
 class AstronomyShowViewSet(viewsets.ModelViewSet):
     queryset = AstronomyShow.objects.prefetch_related("show_theme")
     serializer_class = AstronomyShowSerializer
+    permission_classes = (IsAdminOrIfAuthenticatedReadOnly, )
 
     def get_serializer_class(self):
         if self.action == "list":
@@ -50,6 +61,7 @@ class AstronomyShowViewSet(viewsets.ModelViewSet):
 class ShowSessionViewSet(viewsets.ModelViewSet):
     queryset = ShowSession.objects.select_related("astronomy_show", "planetarium_dome")
     serializer_class = ShowSessionSerializer
+    permission_classes = (IsAdminOrIfAuthenticatedReadOnly, )
 
     def get_serializer_class(self):
         if self.action == "list":
@@ -64,6 +76,7 @@ class ShowSessionViewSet(viewsets.ModelViewSet):
 class ReservationViewSet(viewsets.ModelViewSet):
     queryset = Reservation.objects.prefetch_related("tickets")
     serializer_class = ReservationSerializer
+    permission_classes = (IsAuthenticated, )
 
     def get_queryset(self):
         queryset = self.queryset
