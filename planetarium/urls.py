7a7a62f5e40d1205ef7416627ff09cf6e5bac0a1
Kostia <kostia3507@gmail.com>
2023-12-11 13:49:39 +0200

    added astronomy show view set to urls file

--- planetarium/urls.py
+++ planetarium/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from planetarium.views import PlanetariumDomeViewSet, ShowThemeViewSet
+from planetarium.views import PlanetariumDomeViewSet, ShowThemeViewSet, AstronomyShowViewSet
 
 router = routers.DefaultRouter()
 
 router.register("planetarium_dome", PlanetariumDomeViewSet)
 router.register("show_theme", ShowThemeViewSet)
+router.register("astronomy_show", AstronomyShowViewSet)
 
 urlpatterns = [
     path("", include(router.urls))
