df78d08f2ee8ec3b180dd01e096c1ed981032d73
Kostia <kostia3507@gmail.com>
2023-12-11 22:06:28 +0200

    added class meta with ordering variable to show session model

--- planetarium/models.py
+++ planetarium/models.py
@@ -61,6 +61,9 @@ class ShowSession(models.Model):
     )
     show_time = models.DateTimeField()
 
+    class Meta:
+        ordering = ["-show_time"]
+
     @property
     def tickets_available(self):
         return self.planetarium_dome.capacity - self.tickets.count()
