88e497e1603f241acd1fdc3eaae43669936b6182
Kostia <kostia3507@gmail.com>
2023-12-11 18:29:14 +0200

    added reservation view set to urls

--- planetarium/urls.py
+++ planetarium/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from planetarium.views import PlanetariumDomeViewSet, ShowThemeViewSet, AstronomyShowViewSet, ShowSessionViewSet
+from planetarium.views import PlanetariumDomeViewSet, ShowThemeViewSet, AstronomyShowViewSet, ShowSessionViewSet, \
+    ReservationViewSet
 
 router = routers.DefaultRouter()
 
@@ -9,6 +10,7 @@ router.register("planetarium_dome", PlanetariumDomeViewSet)
 router.register("show_theme", ShowThemeViewSet)
 router.register("astronomy_show", AstronomyShowViewSet)
 router.register("show_session", ShowSessionViewSet)
+router.register("reservation", ReservationViewSet)
 
 urlpatterns = [
     path("", include(router.urls))
