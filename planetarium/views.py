1ad925dff86fc015db2a5eb42db43b44ef8b6308
Kostia <kostia3507@gmail.com>
2023-12-11 13:49:19 +0200

    added viewset for astronomy show

--- planetarium/views.py
+++ planetarium/views.py
@@ -1,7 +1,8 @@
 from rest_framework import viewsets, mixins
 
-from .models import PlanetariumDome, ShowTheme
-from .serializers import PlanetariumDomeSerializer, ShowThemeSerializer
+from .models import PlanetariumDome, ShowTheme, AstronomyShow
+from .serializers import PlanetariumDomeSerializer, ShowThemeSerializer, AstronomyShowSerializer, \
+    AstronomyShowListSerializer, AstronomyShowDetailSerializer
 
 
 class PlanetariumDomeViewSet(
@@ -20,3 +21,17 @@ class ShowThemeViewSet(
 ):
     queryset = ShowTheme.objects.all()
     serializer_class = ShowThemeSerializer
+
+
+class AstronomyShowViewSet(viewsets.ModelViewSet):
+    queryset = AstronomyShow.objects.prefetch_related("show_theme")
+    serializer_class = AstronomyShowSerializer
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return AstronomyShowListSerializer
+
+        if self.action == "retrieve":
+            return AstronomyShowDetailSerializer
+
+        return AstronomyShowSerializer
