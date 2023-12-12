e12e15c26dd055a4435da240f580a5de3808283e
Kostia <kostia3507@gmail.com>
2023-12-12 14:51:47 +0200

    reformatted urls file in planetarium app

--- planetarium/urls.py
+++ planetarium/urls.py
@@ -1,16 +1,21 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from planetarium.views import PlanetariumDomeViewSet, ShowThemeViewSet, AstronomyShowViewSet, ShowSessionViewSet, \
+from planetarium.views import (
+    PlanetariumDomeViewSet,
+    ShowThemeViewSet,
+    AstronomyShowViewSet,
+    ShowSessionViewSet,
     ReservationViewSet
+)
 
 router = routers.DefaultRouter()
 
-router.register("planetarium_dome", PlanetariumDomeViewSet)
-router.register("show_theme", ShowThemeViewSet)
-router.register("astronomy_show", AstronomyShowViewSet)
-router.register("show_session", ShowSessionViewSet)
-router.register("reservation", ReservationViewSet)
+router.register("planetarium-domes", PlanetariumDomeViewSet)
+router.register("show-themes", ShowThemeViewSet)
+router.register("astronomy-shows", AstronomyShowViewSet)
+router.register("show-sessions", ShowSessionViewSet)
+router.register("reservations", ReservationViewSet)
 
 urlpatterns = [
     path("", include(router.urls))
